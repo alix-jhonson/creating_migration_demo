b238ee83b7fc1ea426fa00b76e38ac3663769e36
rana haseeb <alix@yahoo.com>
2022-06-22 16:16:00 +0500

    referencing in database migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220622110122) do
+ActiveRecord::Schema.define(version: 20220622111338) do
 
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "products", force: :cascade do |t|
   end
