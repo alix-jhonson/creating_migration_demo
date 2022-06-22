f91d2b693b831d79352ad25f3e58533bbfecbc36
rana haseeb <alix@yahoo.com>
2022-06-22 12:48:16 +0500

    up_down_migration_method

--- db/migrate/20220622065625_change_products_price.rb
+++ db/migrate/20220622065625_change_products_price.rb
@@ -1,10 +1,13 @@
 class ChangeProductsPrice < ActiveRecord::Migration
-  def change
-    reversible do |dir|
-      change_table :products do |t|
-        dir.up   { t.change :price, :string }
-        dir.down { t.change :price, :integer }
-      end
+ def up
+    change_table :products do |t|
+      t.change :price, :string
+    end
+  end
+
+  def down
+    change_table :products do |t|
+      t.change :price, :integer
     end
   end
 end
