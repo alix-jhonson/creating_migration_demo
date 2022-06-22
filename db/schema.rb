87cd963e5233b5d4082585b45918a5cc95cfa621
rana haseeb <alix@yahoo.com>
2022-06-22 15:38:28 +0500

    adding column in database migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220622075704) do
+ActiveRecord::Schema.define(version: 20220622093736) do
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "part_number"
+  end
 
   create_table "products", force: :cascade do |t|
   end
